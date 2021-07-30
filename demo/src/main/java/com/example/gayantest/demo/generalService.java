f35373ece46ce11962951edc1f7a16c2a7194127
gayana-iil <gayan.a@informaticsint.com>
2021-07-30 17:04:32 +0530

    modified code

--- demo/src/main/java/com/example/gayantest/demo/generalService.java
+++ demo/src/main/java/com/example/gayantest/demo/generalService.java
@@ -3,6 +3,10 @@ package com.example.gayantest.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * The type Booking service.
  */
@@ -11,15 +15,31 @@ import org.springframework.stereotype.Service;
 public class generalService {
 
  public String getFiles(String reqId){
+
+     List names = new ArrayList();
+     names.add(0,"Gayan");
+     names.add(1,"Prabhashi");
+     names.add(2,"Prasad");
+
+     Optional getName = Optional.of(names.get(0));
+     Optional getName1 = Optional.of(names.get(1));
+     Optional getName2 = Optional.of(names.get(1));
+
      String name = "Gayan";
      if(reqId.equals("1")) {
-         name = "Gayan";
+         if(getName.isPresent()) {
+             name = names.get(0).toString();
+         }
      }
      else if(reqId.equals("2")){
-         name = "Prabhashi";
+         if(getName1.isPresent()) {
+             name = names.get(1).toString();
+         }
      }
      else if(reqId.equals("3")){
-         name = "Prasad";
+         if(getName2.isPresent()) {
+             name = names.get(2).toString();
+         }
      }
      else{
          name = "Invalid User";
